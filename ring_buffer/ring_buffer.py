06baf2e0482c43c1fc333b570acf18839e1d2bfd
brettmadrid <brettmadrid9@gmail.com>
2020-01-12 20:52:15 -0800

    added 'get' method to RingBuffer class

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,19 +1,20 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
-
-  def append(self, item):
-    # remove the oldest value
-    self.storage.pop(self.current)
-    # replace it with the next value
-    self.storage.insert(self.current, item)
-    # iterate to next oldest
-    if self.current < self.capacity-1:
-        self.current += 1
-    else:
+    def __init__(self, capacity):
+        self.capacity = capacity
         self.current = 0
+        self.storage = [None]*capacity
 
-  def get(self):
-    pass
+    def append(self, item):
+        # remove the oldest value
+        self.storage.pop(self.current)
+        # replace it with the next value
+        self.storage.insert(self.current, item)
+        # iterate to next oldest
+        if self.current < self.capacity-1:
+            self.current += 1
+        else:
+            self.current = 0
+
+    def get(self):
+        # return all not None values
+        return [x for x in self.storage if x is not None]
